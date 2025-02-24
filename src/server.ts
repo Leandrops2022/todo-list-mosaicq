630ac7e4a5feb1b20e3fc5dda72a37fa90f6e78a
Leandrops2022 <leandropereirasoares1987@gmail.com>
2025-02-23 21:54:43 -0300

    added cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import appRouter from './routes/routes';
 import { errorHandler } from './middleware/errorHandler';
+import cors from 'cors';
 
 const app = express();
-
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 app.use(express.json());
 
 app.use('/api', appRouter);
